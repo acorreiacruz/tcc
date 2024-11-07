88153705984960655ce3a02c8012d56fff21fc56
acorreiacruz <antoniocruzjunior@outlook.com>
2024-11-07 20:57:57 -0300

    update: order aggregate, renaming file.

---
+++ src/orders/domain/entity/order.ts
@@ -0,0 +1,166 @@
+import Item from "./Item";
+import OrderItem from "./orderItem";
+import crypto from "crypto";
+
+export default class Order {
+    private orderId: string;
+    private userId: string;
+    private orderDate: Date;
+    private status: string;
+    private orderItems: OrderItem[];
+    private fulfillmentMethod: string;
+    private paymentMethod: string;
+    private total: number;
+
+    private constructor(
+        orderId: string,
+        userId: string,
+        orderDate: Date,
+        status: string,
+        fulfillmentMethod: string,
+        paymentMethod: string,
+        total: number
+    ) {
+        if (
+            !Object.values(PaymentMethod).includes(
+                paymentMethod as PaymentMethod
+            )
+        ) {
+            throw new Error(
+                `Invalid payment method. It only is possible: ${Object.values(
+                    PaymentMethod
+                ).join(", ")}`
+            );
+        }
+        if (
+            !Object.values(FulfillmentMethod).includes(
+                fulfillmentMethod as FulfillmentMethod
+            )
+        ) {
+            throw new Error(
+                `Invalid payment method. It only is possible: ${Object.values(
+                    FulfillmentMethod
+                ).join(", ")}`
+            );
+        }
+        if (!Object.values(Status).includes(status as Status)) {
+            throw new Error(
+                `Invalid status value. It only is possible: ${Object.values(
+                    Status
+                ).join(", ")}`
+            );
+        }
+        if (total < 0)
+            throw new Error(
+                "The total value of a order can't be a negative number"
+            );
+        this.orderDate = orderDate;
+        this.userId = userId;
+        this.orderId = orderId;
+        this.status = status;
+        this.orderItems = [];
+        this.paymentMethod = paymentMethod;
+        this.fulfillmentMethod = fulfillmentMethod;
+        this.total = total;
+    }
+
+    getTotal(): number {
+        return this.total;
+    }
+
+    getFulfillmentMethod(): string {
+        return this.fulfillmentMethod;
+    }
+
+    getStatus(): string {
+        return this.status;
+    }
+
+    getOrderId(): string {
+        return this.orderId;
+    }
+
+    getUserId(): string {
+        return this.userId;
+    }
+
+    getOrderItems(): OrderItem[] {
+        return this.orderItems;
+    }
+
+    getPaymentMethod(): string {
+        return this.paymentMethod;
+    }
+
+    addItem(item: Item, quantity: number): void {
+        this.orderItems.push(
+            new OrderItem(item.getId(), quantity, item.getPrice())
+        );
+        this.total += item.getPrice() * quantity;
+    }
+
+
+    static create(
+        userId: string,
+        orderDate: Date,
+        fulfillmentMethod: string,
+        paymentMethod: string
+    ): Order {
+        return new Order(
+            crypto.randomUUID(),
+            userId,
+            orderDate,
+            "pending",
+            fulfillmentMethod,
+            paymentMethod,
+            0
+        );
+    }
+
+    fail() {
+        this.status = "failed";
+    }
+
+    static restore(
+        orderId: string,
+        userId: string,
+        orderDate: string,
+        status: string,
+        fulfillmentMethod: string,
+        paymentMethod: string,
+        total: number
+    ): Order {
+        return new Order(
+            orderId,
+            userId,
+            new Date(orderDate),
+            status,
+            fulfillmentMethod,
+            paymentMethod,
+            total
+        );
+    }
+
+}
+
+enum Status {
+    Pending = "pending",
+    Confirmed = "confirmed",
+    InPreparation = "in_preparation",
+    Ready = "ready",
+    OutForDelivery = "out_for_delivery",
+    Concluded = "concluded",
+    Canceled = "canceled",
+    DeliveryFailed = "delivery_failed",
+}
+
+enum FulfillmentMethod {
+    Delivery = "delivery",
+    Withdrawal = "withdrawal",
+}
+
+enum PaymentMethod {
+    CreditCard = "credit_card",
+    Pix = "pix",
+    DebitCard = "debit_card",
+}
