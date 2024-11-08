1249a5f9bde87079a658fc06bedc23318e98a810
acorreiacruz <antoniocruzjunior@outlook.com>
2024-11-08 09:23:08 -0300

    feat: item entity

---
+++ src/orders/domain/entity/item.ts
@@ -0,0 +1,56 @@
+import crypto from "crypto";
+
+export default class Item {
+    private itemId: string;
+    private name: string;
+    private description: string;
+    private price: number;
+
+    constructor(
+        itemId: string,
+        name: string,
+        description: string,
+        price: number
+    ) {
+        if (price <= 0)
+            throw new Error(
+                "The item price must be a strictly positive number"
+            );
+        if (description.length > 300) {
+            throw new Error("Item description has a maximum of 300 characters");
+        }
+        this.itemId = itemId;
+        this.price = price;
+        this.name = name;
+        this.description = description;
+    }
+
+    getName(): string {
+        return this.name;
+    }
+
+    getPrice(): number {
+        return this.price;
+    }
+
+    getId(): string {
+        return this.itemId;
+    }
+
+    getDescription(): string {
+        return this.description;
+    }
+
+    static create(name: string, description: string, price: number): Item {
+        return new Item(crypto.randomUUID(), name, description, price);
+    }
+
+    static restore(
+        itemId: string,
+        name: string,
+        description: string,
+        price: number
+    ): Item {
+        return new Item(itemId, name, description, price);
+    }
+}
