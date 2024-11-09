1d610e76f23a0f476c68871da952cd073402ec17
acorreiacruz <antoniocruzjunior@outlook.com>
2024-11-09 12:23:38 -0300

    update: including DomainEnvent parameter in update method of OrderRepository

--- src/orders/domain/repository/orderRepository.ts
+++ src/orders/domain/repository/orderRepository.ts
@@ -1,7 +1,8 @@
 import Order from "../entity/order";
+import DomainEvent from "../event/domainEvent";
 
 export default interface OrderRepository {
     getById(orderId: string): Promise<Order>;
-    create(order: Order): Promise<void>;
-    update(order: Order): Promise<void>;
+    create(order: Order, event: DomainEvent): Promise<void>;
+    update(order: Order, event: DomainEvent): Promise<void>;
 }
