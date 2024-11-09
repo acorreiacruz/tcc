4439aed2e4226d37aaa9fef528838ede0ed8bb4b
acorreiacruz <antoniocruzjunior@outlook.com>
2024-11-09 12:30:02 -0300

    fix: status and getByStatus types from String to string

--- src/orders/domain/repository/outboxRepository.ts
+++ src/orders/domain/repository/outboxRepository.ts
@@ -2,5 +2,5 @@ import DomainEvent from "../event/domainEvent";
 
 export default interface OutboxRepository {
     create(event: DomainEvent): Promise<void>;
-    getByStatus(status: String[]): Promise<String[]>;
+    getByStatus(status: string[]): Promise<string[]>;
 }
