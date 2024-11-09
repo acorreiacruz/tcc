c8bf658401cdffea3c1e1bb1a77b48fe0b4c5e79
acorreiacruz <antoniocruzjunior@outlook.com>
2024-11-09 16:10:46 -0300

    feat: PlaceOrder use case

---
+++ src/orders/application/use_case/placeOrder.ts
@@ -0,0 +1,52 @@
+import Item from "../../domain/entity/item";
+import Order from "../../domain/entity/order";
+import OrderPlaced from "../../domain/event/orderPlaced";
+import ItemRepository from "../../domain/repository/itemRepository";
+import OrderRepository from "../../domain/repository/orderRepository";
+
+export default class PlaceOrder {
+    private orderRepository: OrderRepository;
+    private itemRepository: ItemRepository;
+
+    constructor(
+        orderRepository: OrderRepository,
+        itemRepository: ItemRepository
+    ) {
+        this.orderRepository = orderRepository;
+        this.itemRepository = itemRepository;
+    }
+
+    async execute(command: PlaceOrderCommand): Promise<PlaceOrderOutput> {
+        const order: Order = Order.create(
+            command.userId,
+            new Date(command.orderDate),
+            command.fulfillmentMethod,
+            command.paymentMethod
+        );
+        const items: Item[] = await this.itemRepository.getByIds(
+            Object.keys(command.orderItems)
+        );
+        for (const item of items) {
+            order.addItem(item, command.orderItems[item.getId()].quantity);
+        }
+        const orderPlaced = OrderPlaced.create(order);
+        await this.orderRepository.create(order, orderPlaced);
+        return {
+            orderId: order.getId(),
+            status: "pending"
+        }
+    }
+}
+
+type PlaceOrderCommand = {
+    userId: string;
+    orderDate: string;
+    fulfillmentMethod: string;
+    paymentMethod: string;
+    orderItems: { [itemId: string]: { quantity: number } };
+};
+
+type PlaceOrderOutput = {
+    orderId: string,
+    status: string
+}
