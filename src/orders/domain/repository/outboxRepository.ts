32a4f577c9fdffe9b85f48cd446b1324456bfaa6
acorreiacruz <antoniocruzjunior@outlook.com>
2024-11-10 20:30:15 -0300

    feat: update method in OutboxRepository interface

--- src/orders/domain/repository/outboxRepository.ts
+++ src/orders/domain/repository/outboxRepository.ts
@@ -1,6 +1,8 @@
+import Outbox from "../../application/outbox";
 import DomainEvent from "../event/domainEvent";
 
 export default interface OutboxRepository {
     create(event: DomainEvent): Promise<void>;
-    getByStatus(status: string[]): Promise<string[]>;
+    getByStatus(status: string[]): Promise<Outbox[]>;
+    update(outboxes: Outbox[]): Promise<void>;
 }
