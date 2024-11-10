ecc8a834d5e49f9acde07867b968598d2cceba3c
acorreiacruz <antoniocruzjunior@outlook.com>
2024-11-10 20:10:09 -0300

    feat: MessageBroker interface adapter

---
+++ src/orders/infraestructure/messageBroker.ts
@@ -0,0 +1,9 @@
+export default interface MessageBroker {
+    connect(): Promise<void>;
+    publish(
+        message: string,
+        exchange: string,
+        routingKey: string
+    ): Promise<string>;
+    consume(queue: string, callback: Function): Promise<void>;
+}
