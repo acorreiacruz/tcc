60370b0b7082d35674b791c6f9845d7db902c739
acorreiacruz <antoniocruzjunior@outlook.com>
2024-11-14 16:08:23 -0300

    update: refactoring DomainEvent from interface to abstract class

--- src/orders/domain/event/domainEvent.ts
+++ src/orders/domain/event/domainEvent.ts
@@ -1,4 +1,4 @@
-export default interface DomainEvent {
+export default abstract class DomainEvent {
     eventId: string;
     entityId: string;
     correlationId: string;
@@ -7,5 +7,33 @@ export default interface DomainEvent {
     source: string;
     payload: Object;
 
-    toJSON(): Object;
+    protected constructor(
+        eventId: string,
+        entityId: string,
+        correlationId: string,
+        name: string,
+        timestamp: Date,
+        source: string,
+        payload: Object
+    ) {
+        this.eventId = eventId;
+        this.entityId = entityId;
+        this.correlationId = correlationId;
+        this.name = name;
+        this.source = source;
+        this.payload = payload;
+        this.timestamp = timestamp;
+    }
+
+    toJSON(): any {
+        return {
+            eventId: this.eventId,
+            correlationId: this.correlationId,
+            entityId: this.entityId,
+            name: this.name,
+            timestamp: this.timestamp,
+            source: this.source,
+            payload: this.payload,
+        };
+    }
 }
