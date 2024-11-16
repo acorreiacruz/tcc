71ccbc4ccc57afb56c13c62d8b6b4c62dc106494
acorreiacruz <antoniocruzjunior@outlook.com>
2024-11-15 22:08:07 -0300

    update: including RabbitMQ routing information for order events in the credentials file

--- src/orders/settings/credentials.ts
+++ src/orders/settings/credentials.ts
@@ -6,3 +6,11 @@ export const RABBITMQ_AMQP_PORT = process.env.RABBITMQ_AMQP_PORT || "invalid";
 export const RABBITMQ_USERNAME = process.env.RABBITMQ_USERNAME || "invalid";
 export const RABBITMQ_PASSWORD = process.env.RABBITMQ_PASSWORD || "invalid";
 export const RABBITMQ_HOST = process.env.RABBITMQ_HOST || "invalid";
+
+export const ORDER_PLACED_EXCHANGE = process.env.ORDER_PLACED_EXCHANGE || "invalid";
+export const ORDER_PLACED_ROUTING_KEY = process.env.ORDER_PLACED_ROUTING_KEY || "invalid";
+export const ORDER_CANCELED_EXCHANGE = process.env.ORDER_PLACED_EXCHANGE || "invalid";
+export const ORDER_CANCELED_ROUTING_KEY = process.env.ORDER_PLACED_ROUTING_KEY || "invalid";
+export const ORDER_UPDATED_EXCHANGE = process.env.ORDER_PLACED_EXCHANGE || "invalid";
+export const ORDER_UPDATED_ROUTING_KEY = process.env.ORDER_PLACED_ROUTING_KEY || "invalid";
+
