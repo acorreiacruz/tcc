3976540ae6f3cbd802c0e0ce4e4483f59b4262a9
acorreiacruz <antoniocruzjunior@outlook.com>
2024-11-16 10:28:11 -0300

    feat: ConfirmOrder use case

---
+++ src/orders/application/use_case/confirmOrder.ts
@@ -0,0 +1,28 @@
+import OrderStatusUpdated from "../../domain/event/orderStatusUpdated";
+import OrderRepository from "../../domain/repository/orderRepository";
+
+export default class ConfirmOrder {
+    constructor(private orderRepository: OrderRepository) {}
+    async execute(command: ConfirmOrderCommand): Promise<ConfirmOrderOutput> {
+        const order = await this.orderRepository.getById(command.orderId);
+        if (command.userId !== order.getUserId())
+            throw new Error(
+                "The logged user cannot confirm another user's order"
+            );
+        order.confirm();
+        const orderConfirmed = OrderStatusUpdated.create(order, "OrderConfirmed");
+        await this.orderRepository.update(order, orderConfirmed);
+        return {
+            status: "on_process",
+        };
+    }
+}
+
+export type ConfirmOrderCommand = {
+    orderId: string;
+    userId: string;
+};
+
+export type ConfirmOrderOutput = {
+    status: string;
+};
