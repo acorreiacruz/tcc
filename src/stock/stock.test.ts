519b931678e1ae2dd74c8041cc79fa17dd07c24f
acorreiacruz <antoniocruzjunior@outlook.com>
2024-11-17 13:03:19 -0300

    test: Must not create a Stock with totalQuantity less than zero

--- src/stock/stock.test.ts
+++ src/stock/stock.test.ts
@@ -1,4 +1,7 @@
 import Stock from "./stock";
+import {
+    NegativeStockError,
+} from "./stock.errors";
 
 let itemId = "7043c7e6-0da2-4fa7-ab6e-a966428f7cb4";
 let stockId = "f20f5264-8a7f-4098-a832-bfa9df6ce8c8";
@@ -19,4 +22,12 @@ describe("Testing Stock", () => {
         expect(stock.getTotalQuantity()).toBe(250);
         expect(stock.getItemId()).toBe(itemId);
     });
+
+    test("Must not create a Stock with totalQuantity less than zero", () => {
+        totalQuantity = -1;
+        expect(() => Stock.create(itemId, totalQuantity)).toThrow(
+            new NegativeStockError("totalQuantity")
+        );
+    });
+
     });
