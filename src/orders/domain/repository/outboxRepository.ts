f49486acbd437e0bf22f91aa07029c807d9eaebc
acorreiacruz <antoniocruzjunior@outlook.com>
2024-11-17 22:16:32 -0300

    update: moving OutboxRepository interface file to ./src/orders/infraestructure/repository/

--- src/orders/domain/repository/outboxRepository.ts
+++
@@ -1,9 +0,0 @@
-import Outbox from "../../application/outbox";
-import DomainEvent from "../event/domainEvent";
-
-export default interface OutboxRepository {
-    create(event: DomainEvent): Promise<void>;
-    getByStatus(status: string[]): Promise<Outbox[]>;
-    update(outboxes: Outbox[]): Promise<void>;
-    delete(outboxes: Outbox[]): Promise<void>;
-}
