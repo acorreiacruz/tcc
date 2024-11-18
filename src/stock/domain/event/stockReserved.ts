62b4f6e35cf006da0e5526487bb37097b4461b6a
acorreiacruz <antoniocruzjunior@outlook.com>
2024-11-18 11:44:41 -0300

    update:  defining 'event' as the only parameter of the static create method of the StockReserved

--- src/stock/domain/event/stockReserved.ts
+++ src/stock/domain/event/stockReserved.ts
@@ -1,22 +1,21 @@
 import DomainEvent from "../../../common/domainEvent";
 import crypto from "crypto";
-import { ReserveStockCommand } from "../../application/use_case/reserveStock";
 
 export default class StockReserved extends DomainEvent{
-    static create(command: ReserveStockCommand): StockReserved {
+    static create(event: DomainEvent): StockReserved {
         const eventId = crypto.randomUUID();
         const name = "StockReserved";
         const source = "StockService";
         const timestamp = new Date();
         const payload = {
-            userId: command.userId,
-            orderId: command.orderId,
-            total: command.total,
-            paymentMethod: command.paymentMethod
+            userId: event.payload.userId,
+            orderId: event.payload.orderId,
+            total: event.payload.total,
+            paymentMethod: event.payload.paymentMethod
         }
         return new StockReserved(
             eventId,
-            command.correlationId,
+            event.correlationId,
             name,
             timestamp,
             source,
