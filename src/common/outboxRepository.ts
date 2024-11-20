8aeef021f8e22d341a381461b697175fb9e05784
acorreiacruz <antoniocruzjunior@outlook.com>
2024-11-20 12:20:06 -0300

    update: changing OutboxRepository interface method from update to updateStatus

--- src/common/outboxRepository.ts
+++ src/common/outboxRepository.ts
@@ -4,6 +4,6 @@ import DomainEvent from "./domainEvent";
 export default interface OutboxRepository {
     create(event: DomainEvent): Promise<void>;
     getByStatus(status: string[]): Promise<Outbox[]>;
-    update(outboxes: Outbox[]): Promise<void>;
+    updateStatus(outboxes: Outbox[]): Promise<void>;
     delete(outboxes: Outbox[]): Promise<void>;
 }
