1124892b3a5caf4a1202e81aad3121d965a66f20
acorreiacruz <antoniocruzjunior@outlook.com>
2024-11-20 12:17:32 -0300

    test: StcockOutboxRepository - Must create a outbox from any stock domain event

---
+++ src/stock/tests/repository/stockOutboxRepository.test.ts
@@ -0,0 +1,34 @@
+import DomainEvent from "../../../common/domainEvent";
+import Outbox from "../../../common/outbox";
+import OutboxRepository from "../../../common/outboxRepository";
+import { PrismaClient } from "../../infraestructure/orm/prisma/prisma-client";
+import StockOutboxRepositoryDatabase from "../../infraestructure/repository/stockOutboxRepositoryDatabase";
+import { StockReservedMock } from "../utils/stockDomainEventMock";
+
+const connection: PrismaClient = new PrismaClient();
+const stockOutboxRepository: OutboxRepository =
+    new StockOutboxRepositoryDatabase();
+const stockReservedMock: DomainEvent = new StockReservedMock();
+let stockOutboxes: Outbox[];
+
+describe("Testing StockOutboxRepository", () => {
+
+    beforeEach(async () => {
+        await stockOutboxRepository.create(stockReservedMock);
+    });
+
+    afterEach(async () => {
+        await connection.stockOutbox.deleteMany();
+    });
+
+    test("Must create a outbox from any stock domain event", async () => {
+        stockOutboxes = await stockOutboxRepository.getByStatus(["pending"]);
+        expect(stockOutboxes[0].eventId).toBe(stockReservedMock.eventId);
+        expect(stockOutboxes[0].eventName).toBe(stockReservedMock.name);
+        expect(stockOutboxes[0].payload).toBe(
+            JSON.stringify(stockReservedMock)
+        );
+    });
+
+
+});
