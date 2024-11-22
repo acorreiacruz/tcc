1e1b50bb85c8b350543e12d8a0d139eaf8a85e06
acorreiacruz <antoniocruzjunior@outlook.com>
2024-11-22 15:19:55 -0300

    test: domain events mocks for order tests

--- src/orders/tests/utils/orderDomainEventsMocks.ts
+++ src/orders/tests/utils/orderDomainEventsMocks.ts
@@ -1,7 +1,8 @@
 import DomainEvent from "../../../common/domainEvent";
+import { OrderData } from "./entitiesData";
 
 export class OrderDomainEventMock extends DomainEvent {
-    constructor(){
+    constructor() {
         super(
             "5ef7b80e-4808-4dad-a7a1-8e93fd437fe2",
             "913fefd3-39af-4398-b989-a2a0d48d1bc2",
@@ -10,6 +11,38 @@ export class OrderDomainEventMock extends DomainEvent {
             "OrderService",
             {
                 any: "any",
+            }
+        );
+    }
+}
+
+export class PaymentConfirmedMock extends DomainEvent {
+    constructor(orderData: OrderData) {
+        super(
+            "5ef7b80e-4808-4dad-a7a1-8e93fd437fe2",
+            "913fefd3-39af-4398-b989-a2a0d48d1bc2",
+            "payment_confirmed",
+            new Date(),
+            "payment_confirm_payment",
+            {
+                userId: orderData.userId,
+                orderId: orderData.orderId,
+            }
+        );
+    }
+}
+
+export class DeliveryConcludedMock extends DomainEvent {
+    constructor(orderData: OrderData) {
+        super(
+            "5ef7b80e-4808-4dad-a7a1-8e93fd437fe2",
+            "913fefd3-39af-4398-b989-a2a0d48d1bc2",
+            "delivery_concluded",
+            new Date(),
+            "delivery_conlude_delivery",
+            {
+                orderId: orderData.orderId,
+                userId: orderData.userId,
             }
         );
     }
