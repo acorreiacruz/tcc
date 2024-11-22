ea503ed27c5115e1f0a2f47fce6377d7c72846f0
acorreiacruz <antoniocruzjunior@outlook.com>
2024-11-21 21:27:04 -0300

    refactor: ConfirmOrder use case

--- src/orders/application/use_case/confirmOrder.ts
+++ src/orders/application/use_case/confirmOrder.ts
@@ -1,28 +1,21 @@
+import DomainEvent from "../../../common/domainEvent";
 import OrderStatusUpdated from "../../domain/event/orderStatusUpdated";
 import OrderRepository from "../../infraestructure/repository/orderRepository";
 
 export default class ConfirmOrder {
     constructor(private orderRepository: OrderRepository) {}
-    async execute(command: ConfirmOrderCommand): Promise<ConfirmOrderOutput> {
-        const order = await this.orderRepository.getById(command.orderId);
-        if (command.userId !== order.getUserId())
+    async execute(event: DomainEvent): Promise<void> {
+        const order = await this.orderRepository.getById(event.payload.orderId);
+        if (event.payload.userId !== order.getUserId())
             throw new Error(
                 "The logged user cannot confirm another user's order"
             );
         order.confirm();
-        const orderConfirmed = OrderStatusUpdated.create(order, "OrderConfirmed");
+        const orderConfirmed = OrderStatusUpdated.create(
+            order,
+            "order_confirmed",
+            "order_confirm_order"
+        );
         await this.orderRepository.update(order, orderConfirmed);
-        return {
-            status: "on_process",
-        };
     }
 }
-
-export type ConfirmOrderCommand = {
-    orderId: string;
-    userId: string;
-};
-
-export type ConfirmOrderOutput = {
-    status: string;
-};
