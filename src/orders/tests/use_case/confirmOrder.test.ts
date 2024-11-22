9eca8fe266350ce92596b63255a124155d7cda4b
acorreiacruz <antoniocruzjunior@outlook.com>
2024-11-22 15:19:34 -0300

    test: refactor ConfirmOrder tests

--- src/orders/tests/use_case/confirmOrder.test.ts
+++ src/orders/tests/use_case/confirmOrder.test.ts
@@ -3,71 +3,32 @@ import { PrismaClient } from "../../infraestructure/orm/prisma/prisma-client";
 import OrderRepository from "../../infraestructure/repository/orderRepository";
 import ConfirmOrder from "../../application/use_case/confirmOrder";
 import { PaymentConfirmedMock } from "../utils/orderDomainEventsMocks";
+import {
+    getItemsData,
+    getOrdersData,
+    ItemData,
+    OrderData,
+} from "../utils/entitiesData";
 
 let orderRepository: OrderRepository = new OrderRepositoryDatabase();
 let confirmOrder: ConfirmOrder = new ConfirmOrder(orderRepository);
 let dbClient: PrismaClient = new PrismaClient();
 
-const item1 = {
-    itemId: "78947ecb-bdec-44ec-8574-71911e5fc1c2",
-    name: "Item 1",
-    description: "Description for item 1",
-    price: 25,
-};
-
-const item2 = {
-    itemId: "47d9535b-3297-410a-b766-e200c7cc59c1",
-    name: "Item 2",
-    description: "Description for item 2",
-    price: 35,
-};
-
-const item3 = {
-    itemId: "d1a523a3-1252-4a02-adac-084f3ee57901",
-    name: "Item 3",
-    description: "Description for item 3",
-    price: 55,
-};
-
-const orderData = {
-    orderId: "cbcfb0e5-0648-49ea-a84e-88ab22e79d26",
-    userId: "9b27b243-9281-4e64-b32a-cb3976e1b6e3",
-    orderDate: new Date(),
-    paymentMethod: "credit_card",
-    fulfillmentMethod: "delivery",
-    orderItems: [
-        {
-            itemId: "78947ecb-bdec-44ec-8574-71911e5fc1c2",
-            quantity: 2,
-            price: 25,
-        },
-        {
-            itemId: "47d9535b-3297-410a-b766-e200c7cc59c1",
-            quantity: 3,
-            price: 35,
-        },
-        {
-            itemId: "d1a523a3-1252-4a02-adac-084f3ee57901",
-            quantity: 4,
-            price: 55,
-        },
-    ],
-    total: 375,
-};
-
-const paymentConfirmedMock = new PaymentConfirmedMock();
+const itemsData: ItemData[] = getItemsData(3);
+const orderData: OrderData = getOrdersData(1, itemsData, "pending")[0];
+let paymentConfirmedMock: PaymentConfirmedMock;
 
 describe("Testing ConfirmOrder", () => {
     beforeEach(async () => {
         await dbClient.item.createMany({
-            data: [item1, item2, item3],
+            data: itemsData,
         });
         await dbClient.order.create({
             data: {
                 orderId: orderData.orderId,
                 userId: orderData.userId,
                 orderDate: orderData.orderDate,
-                status: "pending",
+                status: orderData.status,
                 paymentMethod: orderData.paymentMethod,
                 fulfillmentMethod: orderData.fulfillmentMethod,
                 total: orderData.total,
@@ -85,6 +46,7 @@ describe("Testing ConfirmOrder", () => {
     });
 
     test("Must confirm a order", async () => {
+        paymentConfirmedMock = new PaymentConfirmedMock(orderData);
         await confirmOrder.execute(paymentConfirmedMock);
         const order = await dbClient.order.findFirstOrThrow({
             where: {
