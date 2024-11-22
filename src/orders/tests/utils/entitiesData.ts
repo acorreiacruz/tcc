c913119d59fc6d01c119ff6c97bf380f0dabeb94
acorreiacruz <antoniocruzjunior@outlook.com>
2024-11-22 12:08:34 -0300

    feat: mock functions to order test

---
+++ src/orders/tests/utils/entitiesData.ts
@@ -0,0 +1,69 @@
+import crypto from "crypto";
+
+const getRandomInt = (max: number, min: number): number => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+export const getItemsData = (itemsQuantity: number = 3): ItemData[] => {
+    const itemData = [];
+    for (let i = 1; i <= itemsQuantity; i++) {
+        itemData.push({
+            itemId: crypto.randomUUID(),
+            name: `Item ${i}`,
+            description: `Description of Item ${i}`,
+            price: getRandomInt(5, 50),
+        });
+    }
+    return itemData;
+};
+
+export const getOrdersData = (
+    ordersQuantity: number = 1,
+    itemsData: ItemData[],
+    status: string = "confirmed",
+    paymentMethod: string = "credit_card",
+    fulfillmentMethod: string = "delivery"
+): OrderData[] => {
+    const ordersData: OrderData[] = [];
+    const orderItems = itemsData.map((value) => {
+        return {
+            itemId: value.itemId,
+            quantity: getRandomInt(1, 10),
+            price: value.price,
+        };
+    });
+    const total = orderItems.reduce((subTotal, orderItem) => {
+        return (subTotal += orderItem.price * orderItem.quantity);
+    }, 0);
+    for (let i = 1; i <= ordersQuantity; i++) {
+        ordersData.push({
+            orderId: crypto.randomUUID(),
+            userId: crypto.randomUUID(),
+            status: status,
+            orderDate: new Date(),
+            paymentMethod: paymentMethod,
+            fulfillmentMethod: fulfillmentMethod,
+            total: total,
+        });
+    }
+    return ordersData;
+};
+
+export type ItemData = {
+    itemId: string;
+    name: string;
+    description: string;
+    price: number;
+};
+
+export type OrderData = {
+    orderId: string;
+    userId: string;
+    status: string;
+    orderDate: Date;
+    paymentMethod: string;
+    fulfillmentMethod: string;
+    total: number;
+};
