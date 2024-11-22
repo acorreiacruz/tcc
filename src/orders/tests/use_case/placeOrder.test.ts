020a05a292ea4d1c5449fbc603398993664fed09
acorreiacruz <antoniocruzjunior@outlook.com>
2024-11-22 16:07:21 -0300

    test: refactor PlaceOrder test

--- src/orders/tests/use_case/placeOrder.test.ts
+++ src/orders/tests/use_case/placeOrder.test.ts
@@ -1,75 +1,53 @@
 import PlaceOrder from "../../application/use_case/placeOrder";
 import OrderRepositoryDatabase from "../../infraestructure/repository/orderRepositoryDatabase";
 import ItemRepositoryDatabase from "../../infraestructure/repository/itemRepositoryDatabase";
-import Item from "../../domain/entity/item";
 import { PrismaClient } from "../../infraestructure/orm/prisma/prisma-client";
 import OrderRepository from "../../infraestructure/repository/orderRepository";
 import ItemRepository from "../../infraestructure/repository/itemRepository";
-
-let orderRepository: OrderRepository = new OrderRepositoryDatabase();
-let itemRepository: ItemRepository = new ItemRepositoryDatabase();
-let placeOrder: PlaceOrder = new PlaceOrder(orderRepository, itemRepository);
-let connection: PrismaClient = new PrismaClient();
-let item1 = new Item(
-    "78947ecb-bdec-44ec-8574-71911e5fc1c2",
-    "Item 1",
-    "Description for item 1",
-    15.5
-);
-
-let item2 = new Item(
-    "47d9535b-3297-410a-b766-e200c7cc59c1",
-    "Item 2",
-    "Description for item 2",
-    15.5
-);
-
-let item3 = new Item(
-    "d1a523a3-1252-4a02-adac-084f3ee57901",
-    "Item 3",
-    "Description for item 3",
-    15.5
-);
-
-const placeOrderCommand = {
-    userId: "9b27b243-9281-4e64-b32a-cb3976e1b6e3",
-    orderDate: "2024-11-09T15:30:00",
-    paymentMethod: "credit_card",
-    fulfillmentMethod: "delivery",
-    orderItems: {
-        "78947ecb-bdec-44ec-8574-71911e5fc1c2": { quantity: 2 },
-        "47d9535b-3297-410a-b766-e200c7cc59c1": { quantity: 3 },
-        "d1a523a3-1252-4a02-adac-084f3ee57901": { quantity: 4 },
-    },
-};
+import { getItemsData, getOrdersData, getPlaceOrderCommand } from "../utils/entitiesData";
 
 describe("Tesing PlaceOrder use case", () => {
-    beforeAll(async () => {
-        await connection.item.createMany({
-            data: [item1.toJSON(), item2.toJSON(), item3.toJSON()],
+    const orderRepository: OrderRepository = new OrderRepositoryDatabase();
+    const itemRepository: ItemRepository = new ItemRepositoryDatabase();
+    const placeOrder: PlaceOrder = new PlaceOrder(
+        orderRepository,
+        itemRepository
+    );
+    const dbClient: PrismaClient = new PrismaClient();
+    const itemsData = getItemsData(3);
+    const orderData = getOrdersData(1, itemsData, "pending")[0];
+    const placeOrderCommand = getPlaceOrderCommand(orderData);
+
+    beforeEach(async () => {
+        await dbClient.item.createMany({
+            data: itemsData,
         });
     });
 
-    afterAll(async () => {
-        await connection.item.deleteMany();
-        await connection.order.deleteMany();
-        await connection.orderOutbox.deleteMany();
+    afterEach(async () => {
+        await dbClient.item.deleteMany();
+        await dbClient.order.deleteMany();
+        await dbClient.orderOutbox.deleteMany();
     });
 
     test("Must place a order", async () => {
         const placeOrderOutput = await placeOrder.execute(placeOrderCommand);
         expect(placeOrderOutput.status).toBe("on_process");
-        const orderPlaced = await orderRepository.getById(placeOrderOutput.orderId);
-        expect(orderPlaced.getId()).toBe(placeOrderOutput.orderId);
-        expect(orderPlaced.getTotal()).toBe(139.5);
-        const outbox = await connection.orderOutbox.findFirstOrThrow({
+        const order = await dbClient.order.findFirstOrThrow({
+            where: {
+                orderId: placeOrderOutput.orderId,
+            },
+        });
+        expect(order.total).toBe(orderData.total);
+        const outbox = await dbClient.orderOutbox.findFirstOrThrow({
             where: {
                 status: "pending",
-                eventName: "OrderPlaced"
-            }
-        })
+            },
+        });
         const event = JSON.parse(outbox.event);
-        expect(event.payload.orderId).toBe(orderPlaced.getId());
-        expect(event.payload.userId).toBe(orderPlaced.getUserId());
+        expect(event.source).toBe("order_place_order");
+        expect(event.name).toBe("order_placed");
+        expect(event.payload.orderId).toBe(placeOrderOutput.orderId);
+        expect(event.payload.userId).toBe(orderData.userId);
     });
 });
