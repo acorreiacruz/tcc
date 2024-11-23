789d10d0e769332c1a6f391a20fcfe1777665cc6
acorreiacruz <antoniocruzjunior@outlook.com>
2024-11-22 21:16:27 -0300

    update: PrismaClientSingleton inlcuding dotenv.config

--- src/orders/infraestructure/orm/prismaClientSingleton.ts
+++ src/orders/infraestructure/orm/prismaClientSingleton.ts
@@ -1,4 +1,7 @@
 import { PrismaClient } from "./prisma/prisma-client";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 export default class PrismaClientSingleton {
     private static instance: PrismaClient;
