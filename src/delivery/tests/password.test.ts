39823219111385b03e8f1091ce63430776b55a14
acorreiacruz <antoniocruzjunior@outlook.com>
2024-12-07 16:35:57 -0300

    test: Must create a Password

--- src/delivery/tests/password.test.ts
+++ src/delivery/tests/password.test.ts
@@ -12,4 +12,13 @@ describe("Testing Password", () => {
         expect(Password.isStrong(plainPassword)).toBeFalsy();
     });
 
+    test("Must create a Password", async () => {
+        plainPassword = "P@ssword123#";
+        password = await Password.create(plainPassword);
+        const isEqual = await Password.isEqual(
+            plainPassword,
+            password.getValue()
+        );
+        expect(isEqual).toBeTruthy();
+    });
 });
