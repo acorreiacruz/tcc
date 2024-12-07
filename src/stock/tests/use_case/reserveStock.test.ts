6f2552eeb199e52200a1ff485b3128b14d4c320a
acorreiacruz <antoniocruzjunior@outlook.com>
2024-12-07 13:19:52 -0300

    test: refactoring ReserveStock tests

--- src/stock/tests/use_case/reserveStock.test.ts
+++ src/stock/tests/use_case/reserveStock.test.ts
@@ -7,26 +7,30 @@ import StockRepositoryDataBase from "../../infraestructure/repository/stockRepos
 import { ReserveStock } from "../../application/use_case/reserveStock";
 import OutboxRepository from "../../../common/outboxRepository";
 import { OrderPlacedMock } from "../utils/stockDomainEventMock";
-
-const connection: PrismaClient = new PrismaClient();
-const stockRepository: StockRepository = new StockRepositoryDataBase();
-const stockOutboxRepository: OutboxRepository =
-    new StockOutboxRepositoryDatabase();
-const reserveStock: ReserveStock = new ReserveStock(stockRepository);
-const stock1 = Stock.create("83ee283e-94cf-4c78-a4c7-81be95c56e98", 500);
-const stock2 = Stock.create("c91ff52a-8898-428c-a6e0-dc7d9698b245", 1000);
-const orderPlacedMock: OrderPlacedMock = new OrderPlacedMock();
+import PrismaClientSingleton from "../../infraestructure/orm/prisma/prismaClientSingleton";
 
 describe("Testing ReserveStock", () => {
+    const dbClient: PrismaClient = PrismaClientSingleton.getInstance();
+    const stockRepository: StockRepository = new StockRepositoryDataBase();
+    const stockOutboxRepository: OutboxRepository =
+        new StockOutboxRepositoryDatabase();
+    const reserveStock: ReserveStock = new ReserveStock(stockRepository);
+    const stock1 = Stock.create("83ee283e-94cf-4c78-a4c7-81be95c56e98", 500);
+    const stock2 = Stock.create("c91ff52a-8898-428c-a6e0-dc7d9698b245", 1000);
+    const orderPlacedMock: OrderPlacedMock = new OrderPlacedMock();
     beforeEach(async () => {
-        await connection.stock.createMany({
+        await dbClient.stock.createMany({
             data: [stock1.toJSON(), stock2.toJSON()],
         });
     });
 
     afterEach(async () => {
-        await connection.stock.deleteMany();
-        await connection.stockOutbox.deleteMany();
+        await dbClient.stock.deleteMany();
+        await dbClient.stockOutbox.deleteMany();
+    });
+
+    afterAll(async () => {
+        await dbClient.$disconnect();
     });
     test("Must reserve Stocks from OrderPlaced domain event received", async () => {
         const result = await reserveStock.execute(orderPlacedMock);
