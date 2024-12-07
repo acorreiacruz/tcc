88ca712cada44dcdf74a4e3b4b9e0915e8a91639
acorreiacruz <antoniocruzjunior@outlook.com>
2024-12-07 16:48:20 -0300

    feat: InvalidTransitionToOutForDeliveryError

--- src/delivery/src/domain/entity/delivery.errors.ts
+++ src/delivery/src/domain/entity/delivery.errors.ts
@@ -6,3 +6,12 @@ export class InvalidTransitionToAssignedError extends Error {
         this.name = "InvalidTransitionToAssignedError";
     }
 }
+
+export class InvalidTransitionToOutForDeliveryError extends Error {
+    constructor() {
+        super(
+            "It only is possible to change the status from 'assigned' to 'out_for_delivery'"
+        );
+        this.name = "InvalidTransitionToOutForDeliveryError";
+    }
+}
