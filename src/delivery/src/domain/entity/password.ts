3d723ea9855a50a3f0dfec5e02308ccdd037b542
acorreiacruz <antoniocruzjunior@outlook.com>
2024-12-07 16:37:35 -0300

    update: hashPassword, isEqual and create methods in Password value object

--- src/delivery/src/domain/entity/password.ts
+++ src/delivery/src/domain/entity/password.ts
@@ -1,10 +1,33 @@
+import * as bcrypt from "bcrypt";
 
 export class Password {
+    static SaltRounds: number = 10;
     static Format =
         /^(?=.*[A-Z])(?=.*[!@#$)%={+{(&*])(?=.*[0-9])(?=.*[a-z]).{8,}$/;
+    private value: string;
+    private constructor(value: string) {
+        this.value = value;
+    }
 
     static isStrong(value: string): boolean {
         return Password.Format.test(value);
     }
 
+    static async hashPassword(plainPassword: string): Promise<string> {
+        return await bcrypt.hash(plainPassword, Password.SaltRounds);
+    }
+
+    static async isEqual(
+        plainPassword: string,
+        passwordHash: string
+    ): Promise<boolean> {
+        return await bcrypt.compare(plainPassword, passwordHash);
+    }
+
+    static async create(plainPassword: string): Promise<Password> {
+        if (!Password.isStrong(plainPassword)) throw new InvalidPasswordError();
+        const passwordHash = await Password.hashPassword(plainPassword);
+        return new Password(passwordHash);
+    }
+}
 }
