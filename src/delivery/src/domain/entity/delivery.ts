f75090f35b68be1d326168da62c5ad33c4265f29
acorreiacruz <antoniocruzjunior@outlook.com>
2024-12-07 16:43:59 -0300

    feat: Delivery entity

---
+++ src/delivery/src/domain/entity/delivery.ts
@@ -0,0 +1,72 @@
+import { Location } from "./location";
+import crypto from "crypto";
+export class Delivery {
+    private id: string;
+    private orderId: string;
+    private status: DeliveryStatus;
+    private attempts: number;
+    private location: Location;
+    private deliveryPersonId?: string;
+    private startedAt?: Date;
+    private concludedAt?: Date;
+    private constructor(
+        id: string,
+        orderId: string,
+        status: DeliveryStatus,
+        attempts: number,
+        location: Location,
+        deliveryPersonId?: string,
+        startedAt?: Date,
+        concludedAt?: Date
+    ) {
+        this.id = id;
+        this.orderId = orderId;
+        this.status = status;
+        this.attempts = attempts;
+        this.location = location;
+        this.deliveryPersonId = deliveryPersonId;
+        this.startedAt = startedAt;
+        this.concludedAt = concludedAt;
+    }
+
+    getDeliveryPersonId(): string | undefined {
+        return this.deliveryPersonId;
+    }
+
+    getStartedAt(): Date | undefined {
+        return this.startedAt;
+    }
+
+    getConcludedAt(): Date | undefined {
+        return this.concludedAt;
+    }
+
+    getAttempts(): number {
+        return this.attempts;
+    }
+
+    getId(): string {
+        return this.id;
+    }
+
+    getOrderId(): string {
+        return this.orderId;
+    }
+
+    getStatus(): DeliveryStatus {
+        return this.status;
+    }
+
+    getLocation(): Location {
+        return this.location;
+    }
+
+
+    static create(orderId: string, location: Location): Delivery {
+        const id = crypto.randomUUID();
+        const status: DeliveryStatus = "on_hold";
+        const attempts = 0;
+        return new Delivery(id, orderId, status, attempts, location);
+    }
+
+    }
