0f88fe0fa1ff7e510e03323f7da9a38574c48db5
acorreiacruz <antoniocruzjunior@outlook.com>
2024-12-07 16:28:26 -0300

    update: isValidLatitude method in Location

--- src/delivery/src/domain/entity/location.ts
+++ src/delivery/src/domain/entity/location.ts
@@ -3,8 +3,14 @@ export class Location {
     private latitude: number;
     private longitude: number;
     constructor(latitude: number, longitude: number) {
+        if (!Location.isValidLatitude(latitude))
+            throw new InvalidLatitudeError();
         this.latitude = latitude;
         this.longitude = longitude;
+    }
+    
+    static isValidLatitude(value: number): boolean {
+        return value >= -90 && value <= 90;
     }
     getLatitude(): number {
         return this.latitude;
