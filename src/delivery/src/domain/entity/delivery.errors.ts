b99efc173efbf69e2a812ada83ddc1f4412a077d
acorreiacruz <antoniocruzjunior@outlook.com>
2024-12-07 16:48:54 -0300

    feat: InvalidTransitionToFailedError

--- src/delivery/src/domain/entity/delivery.errors.ts
+++ src/delivery/src/domain/entity/delivery.errors.ts
@@ -1,3 +1,12 @@
+export class InvalidTransitionToFailedError extends Error {
+    constructor() {
+        super(
+            "It only is possible to change the status from 'out_for_delivery' to 'failed'"
+        );
+        this.name = "InvalidTransitionToFailedError";
+    }
+}
+
 export class InvalidTransitionToConcludedError extends Error {
     constructor() {
         super(
