0ef19758fcca8451394508075327064f7fb0445e
acorreiacruz <antoniocruzjunior@outlook.com>
2024-12-23 09:37:33 -0300

    feat: DeliveryPersonRepository interface

---
+++ src/delivery/src/infraestructure/repository/deliveryPersonRepository.ts
@@ -0,0 +1,18 @@
+import {
+    DeliveryPerson,
+    DeliveryPersonStatus,
+} from "../../domain/entity/deliveryPerson";
+
+export interface DeliveryPersonRepository {
+    create(deliveryPerson: DeliveryPerson): Promise<void>;
+    getByStatus(status: DeliveryPersonStatus): Promise<DeliveryPerson[]>;
+    getById(id: string): Promise<DeliveryPerson>;
+    update(deliveryPerson: DeliveryPerson): Promise<void>;
+}
+
+export class DeliveryPersonNotFoundError extends Error {
+    constructor() {
+        super("Delivery person not found");
+        this.name = "DeliveryPersonNotFoundError";
+    }
+}
